a2d68c06f19aa5dba83cafa7168ab9e794c0f49c
javier <javier8javier9@gmail.com>
2026-03-31 19:29:46 +0200

    feat: capturar señal SIGINT para un apagado seguro

--- server/index.js
+++ server/index.js
@@ -10,5 +10,14 @@ for (let i = 0; i < args.length; i++) {
     }
 }
 
-// Arrancar motor principal TCP
-startServer(port);
+// Arrancar motor principal TCP y guardar contexto para el apagado
+const tcpContext = startServer(port);
+
+// Escuchar evento del sistema operativo para cuando el profesor use Ctrl+C
+process.on('SIGINT', () => {
+    console.log('\n[!] Señal SIGINT recibida (Ctrl+C). Preparando apagado...');
+    
+    // TODO: Cerrar conexiones limpiamente antes de tirar el proceso
+    console.log('[+] El sistema se ha apagado sin corrupciones.');
+    process.exit(0);
+});
