3219fa425a2def8d33454764239e87755f017edd
Dario Sarasa <136631919+Zintradev@users.noreply.github.com>
2026-05-07 15:48:35 +0200

    Import registerMimeRoutes in index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const { startServer } = require('./tcp_server');
 
 const { setApiKey, registerRoute } = require('./router');
+const { registerMimeRoutes } = require('./premium/mime/MimeRoutes');
+
 // Leemos argumentos de linea de comandos para extraer --port y --api-key
 const args = process.argv.slice(2);
 let port = 3000; // default
